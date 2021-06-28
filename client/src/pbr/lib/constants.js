f452817c27fe97fe4066f8f3605eb5bf8123fc4a
cyclese96 <cyclese96@gmail.com>
2021-06-28 16:26:15 +0700

    fix time

--- client/src/pbr/lib/constants.js
+++ client/src/pbr/lib/constants.js
@@ -158,7 +158,7 @@ export const supportedPools = [
     access: 'Public',
     network: bscNetwork,
     distribution: 'June 28th 2021, 16:15 UTC',
-    startAt: 1624888800,//test data
+    startAt: 1624888815,//test data
     endAt: 1624892400,//test data
     claimAt: 1624896900,//1724378323,//test data
     startDate: "June 28th 2021"
@@ -202,7 +202,7 @@ export const supportedPools = [
     access: 'Private',
     network: bscNetwork,
     distribution: 'June 28th 2021, 16:15 UTC',
-    startAt: 1624888800,//test data
+    startAt: 1624888815,//test data
     endAt: 1624892400,//test data
     claimAt: 1624896900,//test data
     startDate: "June 28th 2021"
