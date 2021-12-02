8b6b436c9894c1ece4b0253bbb46d112d0774d94
Aamir Alam <aamiralam1991@gmail.com>
2021-12-02 14:48:59 +0530

    fix: update purchase timestamps on server

--- server/constants.js
+++ server/constants.js
@@ -28,19 +28,13 @@ const pools = [
         pid: 1,
         network: bscNetwork,
         symbol: 'ARC-PBR',
-        startAt: 1638453600,
+        startAt: 1638453630,
     },
     {
         pid: 1,
         network: bscNetwork,
         symbol: 'SOLC-PBR',
         startAt: 1638972000,
-    },
-    {
-        pid: 1,
-        network: bscNetwork,
-        symbol: 'ASW-PBR',
-        startAt: 1640883600,
     },
     {
         pid: 1,
@@ -52,8 +46,39 @@ const pools = [
         pid: 1,
         network: moonriverNetwork,
         symbol: 'ROAR-PBR',
-        startAt: 1533939514,
+        startAt: 1639231230,
+    },
+    {
+        pid: 1,
+        network: bscNetwork,
+        symbol: 'GFTS-PBR',
+        startAt: 1639836000,
+    },
+    {
+        pid: 1,
+        network: bscNetwork,
+        symbol: 'CALO-PBR',
+        startAt: 1640008800,
+    },
+    {
+        pid: 1,
+        network: bscNetwork,
+        symbol: 'VST-PBR',
+        startAt: 1640786400,
+    },
+    {
+        pid: 1,
+        network: bscNetwork,
+        symbol: 'ASW-PBR',
+        startAt: 1640883600,
+    },
+    {
+        pid: 1,
+        network: bscNetwork,
+        symbol: 'MUSIC-PBR',
+        startAt: 1641996000,
     }
+
 
 ];
 
